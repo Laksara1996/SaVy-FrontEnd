0ad1ec3c6f4d6d5f8a5ef7f150173576d4426cd9
Laksara <40088875+Laksara1996@users.noreply.github.com>
2019-08-26 00:35:41 +0530

    SignUp + Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,106 +1,142 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
+import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { compose } from 'recompose';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import { withFirebase } from '../Firebase/Index';
 
 import * as ROUTES from '../../Constants/Routes';
 
-const useStyles = makeStyles(theme => ({
-    '@global': {
-        body: {
-            backgroundColor: theme.palette.common.white,
-        },
-    },
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
+const paper = {
+    marginTop: 50,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+}
 
-export default function SignIn() {
-    const classes = useStyles();
+const avatar = {
+    margin: 1,
+    backgroundColor: 'red',
+}
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        <Link to={ROUTES.HOME} style= {{color: 'black'}}>
+const form = {
+    width: '100%',
+    marginTop: 10,
+}
+
+const submit = {
+    marginTop: 20,
+    backgroundColor: "lightblue",
+    color: "black"
+}
+
+const SignInPage = () => (
+    <div>
+        <SignInForm />
+    </div>
+);
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null,
+};
+class SignInFormBase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+    }
+    onSubmit = event => {
+        const { email, password } = this.state;
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+        event.preventDefault();
+    };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    render() {
+        const { email, password, error } = this.state;
+        const isInvalid = password === '' || email === '';
+
+        return (
+
+            <Container component="main" maxWidth="xs" >
+                <CssBaseline />
+                <div style={paper}>
+                    <Avatar style={avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form onSubmit={this.onSubmit} style={form}>
+                        <TextField
+                            name="email"
+                            value={email}
+                            onChange={this.onChange}
+                            type="text"
+                            placeholder="Email Address"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            name="password"
+                            value={password}
+                            onChange={this.onChange}
+                            type="password"
+                            placeholder="Password"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <Button
+                            disabled={isInvalid}
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            style={submit}
+                        >
                             Sign In
-                        </Link>
-
-                    </Button>
-                    <Grid container>
+                        </Button>
                         <Grid item>
                             <Link to={ROUTES.SIGN_UP} variant="body2">
-                                Don't have an account? Sign Up
+                                <div style={{ marginTop: 20 }}>
+                                    Don't have an account? Sign Up
+                                </div>
                             </Link>
                         </Grid>
-                    </Grid>
-                </form>
-            </div>
+                        {error && <p>{error.message}</p>}
+                    </form>
+                </div>
+            </Container>
 
-        </Container>
-    );
+        );
+    }
 }
+const SignInForm = compose(
+    withRouter,
+    withFirebase,
+)(SignInFormBase);
+export default SignInPage;
+export { SignInForm };
